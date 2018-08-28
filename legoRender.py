9acc2b307973b255f18f214fbfcd6796d1ceb4cd
William Herald Snyder <will.herald.snyder@gmail.com>
2018-08-28 11:09:36 -0400

    updating blender script for better regression setup

--- legoRender.py
+++ legoRender.py
@@ -3,6 +3,9 @@ import random
 import math
 import time
 
+from math import degrees
+
+
 
 
 millis = lambda: int(round(time.time() * 1000))
@@ -18,7 +21,7 @@ wing = bpy.data.objects['Wing']
 
 camera = bpy.data.objects['Camera']
 
-objects = [pole, brick, wing]
+objects = [pole, wing]#[pole, brick, wing]
 
 mat = bpy.data.materials.new(name="Testmat")
 
@@ -46,13 +49,13 @@ for piece in objects:
     
 random.seed()
 
-for x in range(20000):
+for x in range(2):
     
-    i = random.randint(0,2)
+    i = random.randint(0,1)
     
     piece = objects[i]
     
-    for y in range(3):
+    for y in range(2):
         
         if (y == i):
             objects[y].location = (0,0,0)
@@ -75,14 +78,14 @@ for x in range(20000):
     scene.render.resolution_percentage = 100
             
     scene.render.image_settings.file_format = 'PNG'
-    scene.render.filepath = "/Users/will/projects/legoproj/training_images/test" + str(x) + ".png"
+    scene.render.filepath = "/Users/will/projects/legoproj/regtest/test" + str(x) + ".png"
     bpy.ops.render.render(write_still = 1)
     
-    labels += (str(x) + " " + piece.name + "\n")
+    labels += (str(x) + " " + str(degrees(piece.rotation_euler[2])) + "\n")
 
 
 print("Generated " + str(x+1) + " images in " + str(float(millis() - timestart)/1000.0) + " seconds") 
 
-text_file = open("/Users/will/projects/legoproj/training_images/labels.txt", "w")
+text_file = open("/Users/will/projects/legoproj/regtest/labels.txt", "w")
 text_file.write(labels)
 text_file.close()    
